cf708fb27a8a5af2d6090661cc4670b376738b8a
Tyler Lin <ddkc747051@gmail.com>
2020-06-15 17:04:27 +0800

    更新分頁功能

--- js/all.js
+++ js/all.js
@@ -4,8 +4,10 @@ $(document).ready(function() {
     let str = '';
     let card = document.querySelector('.content');
     let selectZone = document.getElementById('selectZone');
-    let data;
+    let data; //所有資料
     let allZone = new Array;
+    let zoneData = []; 
+    let nowPage = 1;
     getData();
 
     hotZone.addEventListener('click',hotfilter,false); //監聽熱門按鈕
@@ -33,6 +35,7 @@ $(document).ready(function() {
     }
 
     function updateData() {
+        zoneData = [];
         for (let i = 0; i < data.length; i++) {
             const placeName = data[i].Name;
             const placeImg = data[i].Picture1;
@@ -41,36 +44,41 @@ $(document).ready(function() {
             const placeAdd = data[i].Add;
             const placeTel = data[i].Tel;
             const placeTicketinfo = data[i].Ticketinfo;
-            if (data[i].Zone == str) {
-                card.innerHTML += 
-                `<div class="place_wrap"> 
-                    <div class="img" style="background-image: url(${placeImg})"> 
-                        <div class="bar"> 
-                            <div class="place_name">${placeName}</div> 
-                            <div class="place_zone">${placeZone}</div> 
-                        </div> 
-                    </div> 
-                    <div class="details_wrap"> 
-                        <div class="place_open wrap_set"> 
-                            <span><img src="./img/icons_clock.png" alt=""></span>
-                            ${placeTime}
-                        </div> 
-                        <div class="place_add wrap_set"> 
-                            <span><img src="./img/icons_pin.png" alt=""></span>
-                            ${placeAdd}
-                        </div> 
-                        <div class="place_phone wrap_set">
-                            <span><img src="./img/icons_phone.png" alt=""></span>
-                            ${placeTel}
-                            </div> 
-                        <div class="place_ticket"> 
-                            <span><img src="./img/icons_tag.png" alt=""></span>
-                            ${placeTicketinfo}
-                        </div> 
-                    </div> 
-                </div>`;
-            } 
+
+            if(data[i].Zone == str){
+                zoneData.push(data[i]);
+            }
+            // if (data[i].Zone == str) {
+            //     card.innerHTML += 
+            //     `<div class="place_wrap"> 
+            //         <div class="img" style="background-image: url(${placeImg})"> 
+            //             <div class="bar"> 
+            //                 <div class="place_name">${placeName}</div> 
+            //                 <div class="place_zone">${placeZone}</div> 
+            //             </div> 
+            //         </div> 
+            //         <div class="details_wrap"> 
+            //             <div class="place_open wrap_set"> 
+            //                 <span><img src="./img/icons_clock.png" alt=""></span>
+            //                 ${placeTime}
+            //             </div> 
+            //             <div class="place_add wrap_set"> 
+            //                 <span><img src="./img/icons_pin.png" alt=""></span>
+            //                 ${placeAdd}
+            //             </div> 
+            //             <div class="place_phone wrap_set">
+            //                 <span><img src="./img/icons_phone.png" alt=""></span>
+            //                 ${placeTel}
+            //                 </div> 
+            //             <div class="place_ticket"> 
+            //                 <span><img src="./img/icons_tag.png" alt=""></span>
+            //                 ${placeTicketinfo}
+            //             </div> 
+            //         </div> 
+            //     </div>`;
+            // } 
         }
+        pagination(zoneData,nowPage);
     }
     // 點擊下拉選單更新
     function updateList (e) {
@@ -92,7 +100,110 @@ $(document).ready(function() {
         card.innerHTML='';
         updateData();
     };
+
+
+// 分頁製作---
+
+function pagination (pageData, nowPage) {
+    const dataTotal = pageData.length;
+    const perpage = 6;
+    const pageTotal = Math.ceil(dataTotal / perpage);
+    // console.log(`全部資料:${dataTotal},每一頁顯示:${perpage}筆資料,共有${pageTotal}頁`);
+    let currentPage = nowPage;
+    if (currentPage > pageTotal) {
+        currentPage = pageTotal;
+    }
+    const minData = (currentPage * perpage) - perpage + 1;
+    const maxData = (currentPage * perpage);
+    const newData =[];
+    pageData.forEach((item,index) => {
+        const num = index + 1;
+        if(num >= minData && num <= maxData){
+            newData.push(item);
+        }
+    }) //產生新陣列
+    newData.forEach((item, i )=> {
+        const placeName = newData[i].Name;
+        const placeImg = newData[i].Picture1;
+        const placeZone = newData[i].Zone;
+        const placeTime = newData[i].Opentime;
+        const placeAdd = newData[i].Add;
+        const placeTel = newData[i].Tel;
+        const placeTicketinfo = newData[i].Ticketinfo;
+        card.innerHTML += 
+        `<div class="place_wrap"> 
+            <div class="img" style="background-image: url(${placeImg})"> 
+                <div class="bar"> 
+                    <div class="place_name">${placeName}</div> 
+                    <div class="place_zone">${placeZone}</div> 
+                </div> 
+            </div> 
+            <div class="details_wrap"> 
+                <div class="place_open wrap_set"> 
+                    <span><img src="./img/icons_clock.png" alt=""></span>
+                    ${placeTime}
+                </div> 
+                <div class="place_add wrap_set"> 
+                    <span><img src="./img/icons_pin.png" alt=""></span>
+                    ${placeAdd}
+                </div> 
+                <div class="place_phone wrap_set">
+                    <span><img src="./img/icons_phone.png" alt=""></span>
+                    ${placeTel}
+                    </div> 
+                <div class="place_ticket"> 
+                    <span><img src="./img/icons_tag.png" alt=""></span>
+                    ${placeTicketinfo}
+                </div> 
+            </div> 
+        </div>`;
+    }) 
+    const page = {
+        pageTotal,
+        currentPage,
+        hasPage: currentPage > 1,
+        hasNext: currentPage < pageTotal,
+    }
+    pageBtn(page);
+}
+
+
+function pageBtn (page){
+    let str = '';
+    const total = page.pageTotal;
+  
+    if(page.hasPage) {
+        str += `<li class="page-item"><a class="page-link" href="#" data-page="${Number(page.currentPage) - 1}">Previous</a></li>`;
+    } else {
+        str += `<li class="page-item disabled"><span class="page-link">Previous</span></li>`;
+    }
+  
+
+    for(let i = 1; i <= total; i++){
+        if(Number(page.currentPage) === i) {
+            str +=`<li class="page-item active"><a class="page-link" href="#" data-page="${i}">${i}</a></li>`;
+        } else {
+            str +=`<li class="page-item"><a class="page-link" href="#" data-page="${i}">${i}</a></li>`;
+        }
+    };
+
+    if(page.hasNext) {
+        str += `<li class="page-item"><a class="page-link" href="#" data-page="${Number(page.currentPage) + 1}">Next</a></li>`;
+    } else {
+        str += `<li class="page-item disabled"><span class="page-link">Next</span></li>`;
+    }
+    pageid.innerHTML = str;
+}
+
+function switchPage(e){
+    card.innerHTML="";
+    e.preventDefault();
+    if(e.target.nodeName !== 'A') return;
+    const page = e.target.dataset.page;
+    pagination(zoneData, page);
+}
+
+pageid.addEventListener('click', switchPage);
+
 });
-
-
 
