5768dcb49605aa84c2ce80d4307fde70211f85a7
Tyler Lin <ddkc747051@gmail.com>
2020-06-16 11:59:35 +0800

    變數命名整理

--- js/all.js
+++ js/all.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     let card = document.querySelector('.content');
     let selectZone = document.getElementById('selectZone');
     let data; //所有資料
-    let allZone = new Array;
+    let selectZone = new Array;
     let zoneData = []; 
     let nowPage = 1;
     getData();
@@ -13,7 +13,7 @@ $(document).ready(function() {
     hotZone.addEventListener('click',hotfilter,false); //監聽熱門按鈕
     selectZone.addEventListener('change', updateList , false); //監聽選單
 
-    //1.遠端撈取資料並轉陣列 2.進行篩選顯示在選單 3.設定下方顯示地區初始為三民區並更新資料
+    //1.遠端撈取資料並轉陣列 2.進行篩選顯示在下拉選單 3.設定下方顯示地區初始為三民區並更新資料
     function getData(argument) {
         var xhr = new XMLHttpRequest();  //
         xhr.open('get','https://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97','true')
@@ -21,13 +21,13 @@ $(document).ready(function() {
         xhr.onload =function() {
             data = JSON.parse(xhr.responseText).result.records;
             for (let i = 0; i < data.length; i++) {
-                allZone.push(data[i].Zone);
+                selectZone.push(data[i].Zone);
             }
-            allZone = allZone.filter(function (element, index, self) {
+            selectZone = selectZone.filter(function (element, index, self) {
                 return self.indexOf(element) === index;
             });
-            for (let i = 0; i < allZone.length; i++) {
-                selectZone.innerHTML +='<option value="'+allZone[i]+'">'+allZone[i]+'</option>'
+            for (let i = 0; i < selectZone.length; i++) {
+                selectZone.innerHTML +='<option value="'+selectZone[i]+'">'+selectZone[i]+'</option>'
             }
             str = '三民區';
             updateData();
@@ -102,108 +102,108 @@ $(document).ready(function() {
     };
 
 
-// 分頁製作---
+    // 分頁製作---
 
-function pagination (pageData, nowPage) {
-    card.innerHTML="";
-    const dataTotal = pageData.length;
-    const perpage = 6;
-    const pageTotal = Math.ceil(dataTotal / perpage);
-    // console.log(`全部資料:${dataTotal},每一頁顯示:${perpage}筆資料,共有${pageTotal}頁`);
-    let currentPage = nowPage;
-    if (currentPage > pageTotal) {
-        currentPage = pageTotal;
-    }
-    const minData = (currentPage * perpage) - perpage + 1;
-    const maxData = (currentPage * perpage);
-    const newData =[];
-    pageData.forEach((item,index) => {
-        const num = index + 1;
-        if(num >= minData && num <= maxData){
-            newData.push(item);
+    function pagination (pageData, nowPage) {
+        card.innerHTML="";
+        const dataTotal = pageData.length;
+        const perpage = 6;
+        const pageTotal = Math.ceil(dataTotal / perpage);
+        // console.log(`全部資料:${dataTotal},每一頁顯示:${perpage}筆資料,共有${pageTotal}頁`);
+        let currentPage = nowPage;
+        if (currentPage > pageTotal) {
+            currentPage = pageTotal;
         }
-    }) //產生新陣列
-    newData.forEach((item, i )=> {
-        const placeName = newData[i].Name;
-        const placeImg = newData[i].Picture1;
-        const placeZone = newData[i].Zone;
-        const placeTime = newData[i].Opentime;
-        const placeAdd = newData[i].Add;
-        const placeTel = newData[i].Tel;
-        const placeTicketinfo = newData[i].Ticketinfo;
-        card.innerHTML += 
-        `<div class="place_wrap"> 
-            <div class="img" style="background-image: url(${placeImg})"> 
-                <div class="bar"> 
-                    <div class="place_name">${placeName}</div> 
-                    <div class="place_zone">${placeZone}</div> 
-                </div> 
-            </div> 
-            <div class="details_wrap"> 
-                <div class="place_open wrap_set"> 
-                    <span><img src="./img/icons_clock.png" alt=""></span>
-                    ${placeTime}
-                </div> 
-                <div class="place_add wrap_set"> 
-                    <span><img src="./img/icons_pin.png" alt=""></span>
-                    ${placeAdd}
-                </div> 
-                <div class="place_phone wrap_set">
-                    <span><img src="./img/icons_phone.png" alt=""></span>
-                    ${placeTel}
+        const minData = (currentPage * perpage) - perpage + 1;
+        const maxData = (currentPage * perpage);
+        const newData =[];
+        pageData.forEach((item,index) => {
+            const num = index + 1;
+            if(num >= minData && num <= maxData){
+                newData.push(item);
+            }
+        }) //產生新陣列
+        newData.forEach((item, i )=> {
+            const placeName = newData[i].Name;
+            const placeImg = newData[i].Picture1;
+            const placeZone = newData[i].Zone;
+            const placeTime = newData[i].Opentime;
+            const placeAdd = newData[i].Add;
+            const placeTel = newData[i].Tel;
+            const placeTicketinfo = newData[i].Ticketinfo;
+            card.innerHTML += 
+            `<div class="place_wrap"> 
+                <div class="img" style="background-image: url(${placeImg})"> 
+                    <div class="bar"> 
+                        <div class="place_name">${placeName}</div> 
+                        <div class="place_zone">${placeZone}</div> 
                     </div> 
-                <div class="place_ticket"> 
-                    <span><img src="./img/icons_tag.png" alt=""></span>
-                    ${placeTicketinfo}
                 </div> 
-            </div> 
-        </div>`;
-    }) 
-    const page = {
-        pageTotal,
-        currentPage,
-        hasPage: currentPage > 1,
-        hasNext: currentPage < pageTotal,
-    }
-    pageBtn(page);
-}
-
-
-function pageBtn (page){
-    let str = '';
-    const total = page.pageTotal;
-  
-    if(page.hasPage) {
-        str += `<li class="page-item"><a class="page-link" href="#" data-page="${Number(page.currentPage) - 1}">Previous</a></li>`;
-    } else {
-        str += `<li class="page-item disabled"><span class="page-link">Previous</span></li>`;
-    }
-  
-
-    for(let i = 1; i <= total; i++){
-        if(Number(page.currentPage) === i) {
-            str +=`<li class="page-item active"><a class="page-link" href="#" data-page="${i}">${i}</a></li>`;
-        } else {
-            str +=`<li class="page-item"><a class="page-link" href="#" data-page="${i}">${i}</a></li>`;
+                <div class="details_wrap"> 
+                    <div class="place_open wrap_set"> 
+                        <span><img src="./img/icons_clock.png" alt=""></span>
+                        ${placeTime}
+                    </div> 
+                    <div class="place_add wrap_set"> 
+                        <span><img src="./img/icons_pin.png" alt=""></span>
+                        ${placeAdd}
+                    </div> 
+                    <div class="place_phone wrap_set">
+                        <span><img src="./img/icons_phone.png" alt=""></span>
+                        ${placeTel}
+                        </div> 
+                    <div class="place_ticket"> 
+                        <span><img src="./img/icons_tag.png" alt=""></span>
+                        ${placeTicketinfo}
+                    </div> 
+                </div> 
+            </div>`;
+        }) 
+        const page = {
+            pageTotal,
+            currentPage,
+            hasPage: currentPage > 1,
+            hasNext: currentPage < pageTotal,
         }
-    };
-
-    if(page.hasNext) {
-        str += `<li class="page-item"><a class="page-link" href="#" data-page="${Number(page.currentPage) + 1}">Next</a></li>`;
-    } else {
-        str += `<li class="page-item disabled"><span class="page-link">Next</span></li>`;
+        pageBtn(page);
     }
-    pageid.innerHTML = str;
-}
 
-function switchPage(e){
-    e.preventDefault();
-    if(e.target.nodeName !== 'A') return;
-    const page = e.target.dataset.page;
-    pagination(zoneData, page);
-}
 
-pageid.addEventListener('click', switchPage);
+    function pageBtn (page){
+        let str = '';
+        const total = page.pageTotal;
+      
+        if(page.hasPage) {
+            str += `<li class="page-item"><a class="page-link" href="#" data-page="${Number(page.currentPage) - 1}">Previous</a></li>`;
+        } else {
+            str += `<li class="page-item disabled"><span class="page-link">Previous</span></li>`;
+        }
+      
+
+        for(let i = 1; i <= total; i++){
+            if(Number(page.currentPage) === i) {
+                str +=`<li class="page-item active"><a class="page-link" href="#" data-page="${i}">${i}</a></li>`;
+            } else {
+                str +=`<li class="page-item"><a class="page-link" href="#" data-page="${i}">${i}</a></li>`;
+            }
+        };
+
+        if(page.hasNext) {
+            str += `<li class="page-item"><a class="page-link" href="#" data-page="${Number(page.currentPage) + 1}">Next</a></li>`;
+        } else {
+            str += `<li class="page-item disabled"><span class="page-link">Next</span></li>`;
+        }
+        pageid.innerHTML = str;
+    }
+
+    function switchPage(e){
+        e.preventDefault();
+        if(e.target.nodeName !== 'A') return;
+        const page = e.target.dataset.page;
+        pagination(zoneData, page);
+    }
+
+    pageid.addEventListener('click', switchPage);
 
 });
 
